93277a4c39f596560e0e09eb240510195c5ffbab
Ramuel Gonzalez <ramuelcl@gmail.com>
2021-12-23 22:27:35 +0100

    main

---
+++ resources/views/backend/includes/footer.blade.php
@@ -0,0 +1,16 @@
+<footer class="c-footer">
+  <div>
+    <strong>
+      @lang('Copyright') &copy; {{ date('m.Y') }},
+      <x-utils.link href="mailto:ramuelcl@gmail.com" target="_blank" :text="__(appName())" />
+    </strong>
+
+    @lang('All Rights Reserved')
+  </div>
+
+  <div class="mfs-auto">
+    @lang('Powered by')
+    <x-utils.link href="http://laravel-boilerplate.com" target="_blank" :text="__('laravel-boilerplate.com')" /> &
+    <x-utils.link href="https://coreui.io" target="_blank" text="CoreUI" />
+  </div>
+</footer>
